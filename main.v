1e1b3756a53b8d3792bdb4215c8cfb771436dc1c
Jictyvoo <jictyvoo.ecomp@gmail.com>
2020-02-15 00:30:58 -0300

    Fixed gpio direction and delay time

--- main.v
+++ main.v
@@ -6,13 +6,13 @@ import rpi_gpio
 fn main() {
 	mut gpio := rpi_gpio.Gpio{}
 	if gpio.export_pin("24") {
-		time.sleep(5)
+		time.sleep_ms(5)
 		mut gpio_24 := gpio.get_pin("24")
-		gpio_24.set_direction(false)
-		time.sleep(5)
+		gpio_24.set_direction(true)
+		time.sleep_ms(5)
 		for {
 			println(gpio_24.read())
-			time.sleep(5)
+			time.sleep_ms(5)
 		}
 	}
 }
